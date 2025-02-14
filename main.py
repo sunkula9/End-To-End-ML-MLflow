6510d70b6646e44586965d9ca7726537791cfc0d
sunkula9 <maheshbabu.sunkula@gmail.com>
2025-02-14 17:48:06 +0530

    Model Trainer added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.mlflow import logger
 from mlflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlflow.pipeline.stage_02_data_validation import DataValidationTraningPipeline
 from mlflow.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlflow.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 
@@ -36,3 +37,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Model Trainer Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
